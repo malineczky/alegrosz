430f7d1e439d9c3386c11bbe6f8301e510d167d9
malineczky <aleksandra.m.kot@gmail.com>
2023-07-04 14:55:36 +0200

    json server, product list component

--- src/App.jsx
+++ src/App.jsx
@@ -1,47 +1,9 @@
-import { useState } from "react";
-import { useEffect } from "react";
+import Products from "./components/products";
 
 function App() {
-  const [products, setProducts] = useState([]);
-  const [search, setSearch] = useState("");
-
-  useEffect(() => {
-    getProducts().then((data) => setProducts(data));
-  }, []);
-
-  async function getProducts() {
-    const response = await fetch("http://localhost:3000/products");
-    return await response.json();
-  }
-
   return (
     <>
-      <input
-        type="text"
-        value={search}
-        onChange={() => setSearch(event.target.value)}
-      />
-
-      {products
-        .filter((product) =>
-          `${product.name} ${product.category}`
-            .toLowerCase()
-            .includes(search.toLowerCase())
-        )
-        .map((product) => (
-          <div
-            key={product.id}
-            style={{
-              border: "1px solid black",
-              padding: "10px",
-              margin: "10px",
-            }}
-          >
-            <h2>{product.name}</h2>
-            <p> {product.category}</p>
-            <p>${product.price}</p>
-          </div>
-        ))}
+      <Products />
     </>
   );
 }
