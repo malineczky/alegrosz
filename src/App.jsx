d2661d756c94f63a4b8285c905a4912297f06dc4
malineczky <aleksandra.m.kot@gmail.com>
2023-07-04 13:04:45 +0200

    base

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,49 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from "react";
+import { useEffect } from "react";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [products, setProducts] = useState([]);
+  const [search, setSearch] = useState("");
+
+  useEffect(() => {
+    getProducts().then((data) => setProducts(data));
+  }, []);
+
+  async function getProducts() {
+    const response = await fetch("http://localhost:3000/products");
+    return await response.json();
+  }
 
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <input
+        type="text"
+        value={search}
+        onChange={() => setSearch(event.target.value)}
+      />
+
+      {products
+        .filter((product) =>
+          `${product.name} ${product.category}`
+            .toLowerCase()
+            .includes(search.toLowerCase())
+        )
+        .map((product) => (
+          <div
+            key={product.id}
+            style={{
+              border: "1px solid black",
+              padding: "10px",
+              margin: "10px",
+            }}
+          >
+            <h2>{product.name}</h2>
+            <p> {product.category}</p>
+            <p>${product.price}</p>
+          </div>
+        ))}
     </>
-  )
+  );
 }
 
-export default App
+export default App;
