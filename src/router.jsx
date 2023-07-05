c8d976bd422e33bdc10bc414503424717b3da96f
malineczky <aleksandra.m.kot@gmail.com>
2023-07-05 16:13:27 +0200

    outlet

--- src/router.jsx
+++ src/router.jsx
@@ -3,23 +3,30 @@ import App from "./App.jsx";
 import Product from "./components/Product.jsx";
 import AddProduct from "./components/AddProduct.jsx";
 import EditProduct from "./components/EditProduct.jsx";
+import Layout from "./components/Layout.jsx";
 
 const router = createBrowserRouter([
     {
         path: "/",
-        element: <App />,
-    },
-    {
-        path: "product/:productId",
-        element: <Product />,
-    },
-    {
-        path: "add-product",
-        element: <AddProduct />,
-    },
-    {
-        path: "edit-product/:productId", //po kropce żeby wiedział, jaki produkt ma edytować po id
-        element: <EditProduct />,
+        element: <Layout />,
+        children: [
+            {
+                path: "/",
+                element: <App />,
+            },
+            {
+                path: "product/:productId",
+                element: <Product />,
+            },
+            {
+                path: "add-product",
+                element: <AddProduct />,
+            },
+            {
+                path: "edit-product/:productId", //po kropce żeby wiedział, jaki produkt ma edytować po id
+                element: <EditProduct />,
+            },
+        ],
     },
 ]);
 
