e63b54a9bab08ee4caa83f2200200d2f4c8f5be4
malineczky <aleksandra.m.kot@gmail.com>
2023-07-05 16:58:59 +0200

    material design

--- src/main.jsx
+++ src/main.jsx
@@ -1,11 +1,13 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import { RouterProvider } from "react-router-dom";
-
+import CssBaseline from "@mui/material/CssBaseline";
+import "./index.css";
 import router from "./router.jsx";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
     <React.StrictMode>
+        <CssBaseline />
         <RouterProvider router={router} />
     </React.StrictMode>
 );
